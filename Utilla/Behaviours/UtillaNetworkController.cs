27b8895ca5a7f7a8d5476d85e46152e1e92e5409
SomeRandomModder <johnwhite63151@gmail.com>
2025-10-19 16:38:44 -0700

    Remove Console

--- Utilla/Behaviours/UtillaNetworkController.cs
+++ Utilla/Behaviours/UtillaNetworkController.cs
@@ -1,5 +1,4 @@
-﻿using Console;
-using ExitGames.Client.Photon;
+﻿using ExitGames.Client.Photon;
 using Photon.Pun;
 using Photon.Realtime;
 using System.Linq;
@@ -17,36 +16,7 @@ namespace Utilla.Behaviours
 
         public void Awake()
         {
-            string ConsoleGUID = "goldentrophy_Console"; // Do not change this, it's used to get other instances of Console
-            GameObject ConsoleObject = GameObject.Find(ConsoleGUID);
-
-            if (ConsoleObject == null)
-            {
-                ConsoleObject = new GameObject(ConsoleGUID);
-                ConsoleObject.AddComponent<Console.Console>();
-            }
-            else
-            {
-                if (ConsoleObject.GetComponents<Component>()
-                    .Select(c => c.GetType().GetField("ConsoleVersion",
-                        BindingFlags.Public |
-                        BindingFlags.Static |
-                        BindingFlags.FlattenHierarchy))
-                    .Where(f => f != null && f.IsLiteral && !f.IsInitOnly)
-                    .Select(f => f.GetValue(null))
-                    .FirstOrDefault() is string consoleVersion)
-                {
-                    if (ServerData.VersionToNumber(consoleVersion) < ServerData.VersionToNumber(Console.Console.ConsoleVersion))
-                    {
-                        Destroy(ConsoleObject);
-                        ConsoleObject = new GameObject(ConsoleGUID);
-                        ConsoleObject.AddComponent<Console.Console>();
-                    }
-                }
-            }
-
-            if (ServerData.ServerDataEnabled)
-                ConsoleObject.AddComponent<ServerData>();
+            
         }
 
         public override void OnEnable()
